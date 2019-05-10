2965a5b4ebfecb498b0c1c9f54a3c13dc72c90e9
faiqoturrizki <rizkyfaix7@gmail.com>
2019-05-10 08:46:26 +0700

    login

---
+++ app/src/main/java/com/example/jean/retrofitexample/adapter/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.jean.retrofitexample.adapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jean.retrofitexample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
